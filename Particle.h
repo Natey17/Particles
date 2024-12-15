e4742e426e1290d01a780c5a9ab9af6b4061f854
Nate York <50311051+Natey17@users.noreply.github.com>
2024-12-15 14:51:52 -0800

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -1,259 +1,40 @@
-#include "Particle.h"
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include <cmath>
-using namespace std;
+#pragma once
+#include "Matrices.h"
+#include <SFML/Graphics.hpp>
 
-Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition)
-    : m_A(2, numPoints)
+#define M_PI 3.1415926535897932384626433
+const float G = 1000;      // Gravity
+const float TTL = 5.0;     // Time To Live
+const float SCALE = 0.999;
+
+using namespace Matrices;
+using namespace sf;
+
+class Particle : public Drawable
 {
-    m_ttl = TTL;
-    m_numPoints = numPoints;
+public:
+    Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition);
+    virtual void draw(RenderTarget& target, RenderStates states) const override;
+    void update(float dt);
+    float getTTL() { return m_ttl; }
 
-    float fraction = (float)rand() / (float)RAND_MAX;
-    m_radiansPerSec = fraction * (float)M_PI;
+    //Functions for unit testing
+    bool almostEqual(double a, double b, double eps = 0.0001);
+    void unitTests();
 
-    m_cartesianPlane.setCenter(0, 0);
-    m_cartesianPlane.setSize((float)target.getSize().x, (float)(-1.0 * target.getSize().y));
+private:
+    float m_ttl;
+    int m_numPoints;
+    Vector2f m_centerCoordinate;
+    float m_radiansPerSec;
+    float m_vx;
+    float m_vy;
+    View m_cartesianPlane;
+    Color m_color1;
+    Color m_color2;
+    Matrix m_A;
 
-    Vector2f mapped = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
-    m_centerCoordinate = mapped;
-
-    m_vx = 100.0f + (float)(rand() % 401); // [100,500]
-    if (rand() % 2 == 1) {
-        m_vx *= -1.0f;
-    }
-
-    m_vy = 100.0f + (float)(rand() % 401); // [100,500]
-
-    m_color1 = Color::White;
-    m_color2 = Color((Uint8)(rand() % 256), (Uint8)(rand() % 256), (Uint8)(rand() % 256));
-
-    float theta = ((float)rand() / (float)RAND_MAX) * (float)(M_PI / 2.0f);
-    float dTheta = 2.0f * (float)M_PI / (float)(numPoints - 1);
-
-    for (int j = 0; j < numPoints; j++) {
-        float r = 20.0f + (float)(rand() % 61);
-        float dx = r * cos(theta);
-        float dy = r * sin(theta);
-
-        m_A(0, j) = m_centerCoordinate.x + dx;
-        m_A(1, j) = m_centerCoordinate.y + dy;
-
-        theta += dTheta;
-    }
-}
-
-void Particle::draw(RenderTarget& target, RenderStates states) const
-{
-    VertexArray lines(TriangleFan, m_numPoints + 1);
-
-    Vector2i pixelCenter = target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
-    Vector2f center((float)pixelCenter.x, (float)pixelCenter.y);
-
-    lines[0].position = center;
-    lines[0].color = m_color1;
-
-    for (int j = 1; j <= m_numPoints; j++) {
-        float x = (float)m_A(0, j - 1);
-        float y = (float)m_A(1, j - 1);
-
-        Vector2i pixelPos = target.mapCoordsToPixel(Vector2f(x, y), m_cartesianPlane);
-        lines[j].position = Vector2f((float)pixelPos.x, (float)pixelPos.y);
-        lines[j].color = m_color2;
-    }
-
-    target.draw(lines);
-}
-
-void Particle::update(float dt)
-{
-    m_ttl -= dt;
-
-    rotate(dt * m_radiansPerSec);
-    scale(SCALE);
-
-    float dx = m_vx * dt;
-    m_vy -= G * dt;
-    float dy = m_vy * dt;
-
-    translate(dx, dy);
-}
-
-void Particle::translate(double xShift, double yShift)
-{
-    TranslationMatrix T(xShift, yShift, m_A.getCols());
-    m_A = T + m_A;
-
-    m_centerCoordinate.x += (float)xShift;
-    m_centerCoordinate.y += (float)yShift;
-}
-
-void Particle::rotate(double theta)
-{
-    Vector2f temp = m_centerCoordinate;
-
-    translate(-temp.x, -temp.y);
-
-    RotationMatrix R(theta);
-    m_A = R * m_A;
-
-    translate(temp.x, temp.y);
-}
-
-void Particle::scale(double c)
-{
-    Vector2f temp = m_centerCoordinate;
-
-    translate(-temp.x, -temp.y);
-
-    ScalingMatrix S(c);
-    m_A = S * m_A;
-
-    translate(temp.x, temp.y);
-}
-
-bool Particle::almostEqual(double a, double b, double eps)
-{
-    return fabs(a - b) < eps;
-}
-
-void Particle::unitTests()
-{
-    int score = 0;
-
-    cout << "Testing RotationMatrix constructor...";
-    double theta = M_PI / 4.0;
-    RotationMatrix r(M_PI / 4);
-    if (r.getRows() == 2 && r.getCols() == 2 && almostEqual(r(0, 0), cos(theta))
-        && almostEqual(r(0, 1), -sin(theta))
-        && almostEqual(r(1, 0), sin(theta))
-        && almostEqual(r(1, 1), cos(theta)))
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-    else
-    {
-        cout << "Failed." << endl;
-    }
-
-    cout << "Testing ScalingMatrix constructor...";
-    ScalingMatrix s(1.5);
-    if (s.getRows() == 2 && s.getCols() == 2
-        && almostEqual(s(0, 0), 1.5)
-        && almostEqual(s(0, 1), 0)
-        && almostEqual(s(1, 0), 0)
-        && almostEqual(s(1, 1), 1.5))
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-    else
-    {
-        cout << "Failed." << endl;
-    }
-
-    cout << "Testing TranslationMatrix constructor...";
-    TranslationMatrix t(5, -5, 3);
-    if (t.getRows() == 2 && t.getCols() == 3
-        && almostEqual(t(0, 0), 5)
-        && almostEqual(t(1, 0), -5)
-        && almostEqual(t(0, 1), 5)
-        && almostEqual(t(1, 1), -5)
-        && almostEqual(t(0, 2), 5)
-        && almostEqual(t(1, 2), -5))
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-    else
-    {
-        cout << "Failed." << endl;
-    }
-
-
-    cout << "Testing Particles..." << endl;
-    cout << "Testing Particle mapping to Cartesian origin..." << endl;
-    if (m_centerCoordinate.x != 0 || m_centerCoordinate.y != 0)
-    {
-        cout << "Failed.  Expected (0,0).  Received: (" << m_centerCoordinate.x << "," << m_centerCoordinate.y << ")" << endl;
-    }
-    else
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-
-    cout << "Applying one rotation of 90 degrees about the origin..." << endl;
-    Matrix initialCoords = m_A;
-    rotate(M_PI / 2.0);
-    bool rotationPassed = true;
-    for (int j = 0; j < initialCoords.getCols(); j++)
-    {
-        if (!almostEqual(m_A(0, j), -initialCoords(1, j)) || !almostEqual(m_A(1, j), initialCoords(0, j)))
-        {
-            cout << "Failed mapping: ";
-            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
-            rotationPassed = false;
-        }
-    }
-    if (rotationPassed)
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-    else
-    {
-        cout << "Failed." << endl;
-    }
-
-    cout << "Applying a scale of 0.5..." << endl;
-    initialCoords = m_A;
-    scale(0.5);
-    bool scalePassed = true;
-    for (int j = 0; j < initialCoords.getCols(); j++)
-    {
-        if (!almostEqual(m_A(0, j), 0.5 * initialCoords(0, j)) || !almostEqual(m_A(1, j), 0.5 * initialCoords(1, j)))
-        {
-            cout << "Failed mapping: ";
-            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
-            scalePassed = false;
-        }
-    }
-    if (scalePassed)
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-    else
-    {
-        cout << "Failed." << endl;
-    }
-
-    cout << "Applying a translation of (10, 5)..." << endl;
-    initialCoords = m_A;
-    translate(10, 5);
-    bool translatePassed = true;
-    for (int j = 0; j < initialCoords.getCols(); j++)
-    {
-        if (!almostEqual(m_A(0, j), 10 + initialCoords(0, j)) || !almostEqual(m_A(1, j), 5 + initialCoords(1, j)))
-        {
-            cout << "Failed mapping: ";
-            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
-            translatePassed = false;
-        }
-    }
-    if (translatePassed)
-    {
-        cout << "Passed.  +1" << endl;
-        score++;
-    }
-    else
-    {
-        cout << "Failed." << endl;
-    }
-
-    cout << "Score: " << score << " / 7" << endl;
-}
+    void rotate(double theta);
+    void scale(double c);
+    void translate(double xShift, double yShift);
+};
