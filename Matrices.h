6e9d220833a03e9a6fb5a7b657150eaff8b5f476
SabreenShrestha <145174609+SabreenShrestha@users.noreply.github.com>
2024-12-15 14:22:26 -0800

    Create Matrices.h

---
+++ Matrices.h
@@ -0,0 +1,67 @@
+#ifndef MATRIX_H_INCLUDED
+#define MATRIX_H_INCLUDED
+
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+namespace Matrices
+{
+    class Matrix
+    {
+    public:
+        Matrix(int _rows, int _cols);
+
+        const double& operator()(int i, int j) const
+        {
+            return a.at(i).at(j);
+        }
+
+        double& operator()(int i, int j)
+        {
+            return a.at(i).at(j);
+        }
+
+        int getRows() const { return rows; }
+        int getCols() const { return cols; }
+
+    protected:
+        std::vector<std::vector<double>> a;
+
+    private:
+        int rows;
+        int cols;
+    };
+
+    Matrix operator+(const Matrix& a, const Matrix& b);
+
+    Matrix operator*(const Matrix& a, const Matrix& b);
+
+    bool operator==(const Matrix& a, const Matrix& b);
+
+    bool operator!=(const Matrix& a, const Matrix& b);
+
+    std::ostream& operator<<(std::ostream& os, const Matrix& a);
+
+    class RotationMatrix : public Matrix
+    {
+    public:
+        RotationMatrix(double theta);
+    };
+
+    class ScalingMatrix : public Matrix
+    {
+    public:
+        ScalingMatrix(double scale);
+    };
+
+    class TranslationMatrix : public Matrix
+    {
+    public:
+        TranslationMatrix(double xShift, double yShift, int nCols);
+    };
+
+}
+
+#endif // MATRIX_H_INCLUDED
