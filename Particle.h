1705af0822538a90e0478c1b31eb4983951c7228
Nate York <50311051+Natey17@users.noreply.github.com>
2024-12-15 15:03:53 -0800

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "Matrices.h"
 #include <SFML/Graphics.hpp>
-
-#define M_PI 3.1415926535897932384626433
+#include<cmath>
 const float G = 1000;      // Gravity
 const float TTL = 5.0;     // Time To Live
 const float SCALE = 0.999;
